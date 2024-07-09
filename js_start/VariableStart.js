715f2ba77308ad60f9d78bd40bb5c23e11cdfb05
Jung-YouHwan <yhwjd99@gmail.com>
2024-07-09 16:44:49 +0900

    feat : 자바 스크립트 변수

---
+++ js_start/VariableStart.js
@@ -0,0 +1,22 @@
+// var는 재선언, 재할당 모두 가능해서 잘 안쓰는 듯 합니다.
+var a1 = 1;
+console.log(a1);
+
+var a1 = 2;
+console.log(a1);
+
+a1 = 3;
+console.log(a1);
+
+// let 변수를 선언할 때 사용
+let a2 = 1;
+console.log(a2);
+
+a2 = 2;
+console.log(a2);
+
+// const 상수를 선언할 때 사용
+const a3 = 1;
+console.log(a3);
+
+// a3 = 2; 예외 발생
